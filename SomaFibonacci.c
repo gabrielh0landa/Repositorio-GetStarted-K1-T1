f677377dd126ae447dfe471bfb051013ae32fec4
Gabriel Holanda de Freitas <gabriel.holanda@polealimentos.com.br>
2025-08-14 16:30:44 -0300

    Melhorando nome das variáveis

--- SomaFibonacci.c
+++ SomaFibonacci.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-
 int main(){
   int ultimo_elemt = 1; 
   int penultimo_elemt = 1;
-  int soma = 0;
+  int soma_total = 0;
   int cont = 0;
   
   while(cont <= 4000000){
@@ -14,14 +13,14 @@ int main(){
      ultimo_elemt = cont;
 
      if(cont % 2 == 0){
-       soma = soma + cont;
+       soma_total = soma_total + cont;
      }
     
      printf("%d\n", cont);
     
    }
    
-printf(" Aqui é a soma dos números pares da sequencia de Fibonacci até 4.000.000: %d\n", soma);
+printf(" Aqui é a soma dos números pares da sequencia de Fibonacci até 4.000.000: %d\n", soma_total);
 
  
 }
